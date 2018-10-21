6f27f42dcf4aa4e490eaaa0d36612611f3b9b6aa
Maria Eichman <m.levanuck@gmail.com>
2018-10-21 14:40:07 -0500

    Finished

--- ParseJsonDraft/Program.cs
+++ ParseJsonDraft/Program.cs
@@ -1,12 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ParseJsonDraft
 {
-    class Program
+    public class AutoTradeLinesJson
+    {
+        public Dictionary<string, AutoTradeLinesDetails> AutoTradeLines { get; set; }
+
+    }
+
+    public class AutoTradeLinesDetails
+    {
+        public MatchResults MatchResults { get; set; }
+    }
+
+    public partial class MatchResults
+    {
+        public string Match { get; set; }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string json = @"
+        {
+           'AutoTradeLines':
+
+                    {
+                       'AutoTradeLine1':
+                            {
+                               'Joint':'NUH',
+                               'Poka':'u',
+                               'MonthlyPayment':'$270.00',
+                               'MatchResults':
+                                {
+                                    'Match': 'No'
+                                }
+                            },
+                       'AutoTradeLine2':
+                            {
+                               'Joint':'YES',
+                               'Poka':'u',
+                               'MonthlyPayment':'$270.00',
+                               'MatchResults':
+                                {
+                                    'Match': 'No'
+                                }
+                            },
+                        'AutoTradeLine3':
+                            {
+                               'Joint':'YES',
+                               'Poka':'u',
+                               'MonthlyPayment':'$270.00',
+                               'MatchResults':
+                                {
+                                    'Match': 'Yes'
+                                }
+                            }
+                    }
+
+        }";
+
+
+            AutoTradeLinesJson tradelines = JsonConvert.DeserializeObject<AutoTradeLinesJson>(json);
+
+
+            //Check if Json responce contains "Match": "Yes"
+            foreach(var x in tradelines.AutoTradeLines.Values)
+            {
+                if (x.MatchResults.Match.ToLower() == "yes")
+                {
+                    Console.WriteLine("Test Failed: Result contains Match equal Yes");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
